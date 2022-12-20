710e701bcb51777ff887ecd9e614339718ccbd89
OnlySig <gabriel.castro.pereira11@gmail.com>
2022-12-19 21:02:40 -0300

    CalculadoraReact

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+  padding: 20px;
+  border: 1px solid #CDCDCD;
+  border-radius: 0px;
+  background-color: #e24;
+  font-size: 24px;
+  font-weight: 700;
+  color: #FFFFFF;
+  flex: 1;
+  transition: .3s;
+
+  &:hover {
+    opacity: .6;
+  }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+  height: 75px;
+  background-color: #FFAAAA;
+
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  font-size: 24px;
+
+  input {
+    width: 100%;
+    height: 75px;
+    background-color: #FFAAAA;
+    border: none;
+    align-items: flex-start;
+    padding: 0 10px;
+    color: #FFFFFF;
+    font-size: 24px;
+    }
+`
